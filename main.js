d8c3824cc5c5970060d6c0fa3733cafea17d66c7
Enzo-ads <enzo.vasconcelosofc@gmail.com>
2026-09-17 11:19:16 -0300

    soma

--- main.js
+++ main.js
@@ -1,25 +1,12 @@
-/*Tipos de variáveis
-var -> escopo global
-let -> escopo local
-const -> escopo flex, valor será o mesmo até o fim do programa
-*/
+const formulario = document.getElementById("calcForm")/*pegando o formulário*/
 
-/*criando variavel*/
-var valorUm = 3 
-var valorDois = 19
+formulario.addEventListener("submit", function(e){
+    e.preventDefault();/*impede que a página seja recarregada*/ 
 
-const x = 5
+    const a = Number(document.getElementById("num1").value)
+    const b = Number(document.getElementById("num2").value)
 
-/*
-espremedor (fruta){
-    suco
-}
-*/
+    const soma = a+b
 
-/*função*/
-function somar(a,b){
-    const c = a+b
-    return (c)
-}
-
-console.log(somar(ValorUm,valorDois))
+    document.getElementById("resultado").textContent = soma
+})
