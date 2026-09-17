a99915c13e055e3e6d96479b6a384e5970e4cd7a
Enzo-ads <enzo.vasconcelosofc@gmail.com>
2026-09-17 09:33:47 -0300

    mudancas

---
+++ main.js
@@ -0,0 +1,25 @@
+/*Tipos de variáveis
+var -> escopo global
+let -> escopo local
+const -> escopo flex, valor será o mesmo até o fim do programa
+*/
+
+/*criando variavel*/
+var valorUm = 3 
+var valorDois = 19
+
+const x = 5
+
+/*
+espremedor (fruta){
+    suco
+}
+*/
+
+/*função*/
+function somar(a,b){
+    const c = a+b
+    return (c)
+}
+
+console.log(somar(ValorUm,valorDois))
